f847d42e32be21115b68d7fe558a162013784ca1
mhogrefe <mikhailhogrefe@gmail.com>
2014-12-16 00:04:02 -0500

    added more Interval properties

--- src/test/java/mho/qbar/objects/IntervalProperties.java
+++ src/test/java/mho/qbar/objects/IntervalProperties.java
@@ -3,6 +3,8 @@ package mho.qbar.objects;
 import mho.qbar.iterableProviders.QBarExhaustiveProvider;
 import mho.qbar.iterableProviders.QBarIterableProvider;
 import mho.qbar.iterableProviders.QBarRandomProvider;
+import mho.wheels.structures.Pair;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,13 +12,14 @@ import java.util.Random;
 
 import static mho.qbar.objects.Interval.*;
 import static mho.wheels.iterables.IterableUtils.*;
+import static mho.wheels.ordering.Ordering.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class IntervalProperties {
     private static boolean USE_RANDOM;
     private static final String INTERVAL_CHARS = " (),-/0123456789I[]finty";
-    private static final int SMALL_LIMIT = 1000;
     private static int LIMIT;
 
     private static QBarIterableProvider P;
@@ -39,6 +42,10 @@ public class IntervalProperties {
             USE_RANDOM = useRandom;
             propertiesGetLower();
             propertiesGetUpper();
+            propertiesOf_Rational_Rational();
+            propertiesLessThanOrEqualTo();
+            propertiesGreaterThanOrEqualTo();
+            propertiesOf_Rational();
         }
         System.out.println("Done");
     }
@@ -62,6 +69,65 @@ public class IntervalProperties {
 
         for (Interval a : take(LIMIT, P.intervals())) {
             a.getUpper();
+        }
+    }
+
+    private static void propertiesOf_Rational_Rational() {
+        initialize();
+        System.out.println("\t\ttesting of(Rational, Rational) properties...");
+
+        Iterable<Pair<Rational, Rational>> ps = filter(q -> {
+            assert q.a != null;
+            return le(q.a, q.b);
+        }, P.pairs(P.rationals()));
+        for (Pair<Rational, Rational> p : take(LIMIT, ps)) {
+            assert p.a != null;
+            assert p.b != null;
+            Interval a = of(p.a, p.b);
+            validate(a);
+            assertTrue(a.toString(), a.isFinitelyBounded());
+        }
+    }
+
+    private static void propertiesLessThanOrEqualTo() {
+        initialize();
+        System.out.println("\t\ttesting lessThanOrEqualTo(Rational) properties...");
+
+        for (Rational r : take(LIMIT, P.rationals())) {
+            Interval a = lessThanOrEqualTo(r);
+            validate(a);
+            assertFalse(a.toString(), a.getLower().isPresent());
+            assertTrue(a.toString(), a.getUpper().isPresent());
+        }
+    }
+
+    private static void propertiesGreaterThanOrEqualTo() {
+        initialize();
+        System.out.println("\t\ttesting greaterThanOrEqualTo(Rational) properties...");
+
+        for (Rational r : take(LIMIT, P.rationals())) {
+            Interval a = greaterThanOrEqualTo(r);
+            validate(a);
+            assertTrue(a.toString(), a.getLower().isPresent());
+            assertFalse(a.toString(), a.getUpper().isPresent());
+        }
+    }
+
+    private static void propertiesOf_Rational() {
+        initialize();
+        System.out.println("\t\ttesting of(Rational) properties...");
+
+        for (Rational r : take(LIMIT, P.rationals())) {
+            Interval a = of(r);
+            validate(a);
+            assertTrue(a.toString(), a.isFinitelyBounded());
+            assertEquals(a.toString(), a.diameter().get(), Rational.ZERO);
+        }
+    }
+
+    private static void validate(@NotNull Interval a) {
+        if (a.getLower().isPresent() && a.getUpper().isPresent()) {
+            assertTrue(a.toString(), le(a.getLower().get(), a.getUpper().get()));
         }
     }
 }
