e94a0a171cf901c248073cbe979f2199153e782f
mhogrefe <mikhailhogrefe@gmail.com>
2015-03-21 19:49:54 -0400

    added coefficient tests

--- src/test/java/mho/qbar/objects/PolynomialTest.java
+++ src/test/java/mho/qbar/objects/PolynomialTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static mho.qbar.objects.Polynomial.*;
 import static mho.wheels.iterables.IterableUtils.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class PolynomialTest {
     @Test
@@ -23,6 +24,30 @@ public class PolynomialTest {
         aeq(toList(read("x^2-4*x+7").get()), "[7, -4, 1]");
         aeq(toList(read("x^3-1").get()), "[-1, 0, 0, 1]");
         aeq(toList(read("3*x^10").get()), "[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3]");
+    }
+
+    @Test
+    public void testCoefficient() {
+        aeq(ZERO.coefficient(0), 0);
+        aeq(ZERO.coefficient(5), 0);
+        aeq(ONE.coefficient(0), 1);
+        aeq(ONE.coefficient(5), 0);
+        aeq(X.coefficient(0), 0);
+        aeq(X.coefficient(1), 1);
+        aeq(X.coefficient(5), 0);
+        aeq(read("x^2-4*x+7").get().coefficient(0), 7);
+        aeq(read("x^2-4*x+7").get().coefficient(1), -4);
+        aeq(read("x^2-4*x+7").get().coefficient(2), 1);
+        aeq(read("x^2-4*x+7").get().coefficient(3), 0);
+        aeq(read("x^3-1").get().coefficient(0), -1);
+        aeq(read("x^3-1").get().coefficient(1), 0);
+        aeq(read("x^3-1").get().coefficient(2), 0);
+        aeq(read("x^3-1").get().coefficient(3), 1);
+        aeq(read("x^3-1").get().coefficient(4), 0);
+        try {
+            read("x^3-1").get().coefficient(-1);
+            fail();
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
     }
 
     private static void aeq(Object a, Object b) {
--- src/test/java/mho/qbar/objects/RationalPolynomialTest.java
+++ src/test/java/mho/qbar/objects/RationalPolynomialTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static mho.qbar.objects.RationalPolynomial.*;
 import static mho.wheels.iterables.IterableUtils.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class RationalPolynomialTest {
     @Test
@@ -23,6 +24,30 @@ public class RationalPolynomialTest {
         aeq(toList(read("x^2-7/4*x+1/3").get()), "[1/3, -7/4, 1]");
         aeq(toList(read("x^3-1").get()), "[-1, 0, 0, 1]");
         aeq(toList(read("1/2*x^10").get()), "[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1/2]");
+    }
+
+    @Test
+    public void testCoefficient() {
+        aeq(ZERO.coefficient(0), 0);
+        aeq(ZERO.coefficient(5), 0);
+        aeq(ONE.coefficient(0), 1);
+        aeq(ONE.coefficient(5), 0);
+        aeq(X.coefficient(0), 0);
+        aeq(X.coefficient(1), 1);
+        aeq(X.coefficient(5), 0);
+        aeq(read("x^2-7/4*x+1/3").get().coefficient(0), "1/3");
+        aeq(read("x^2-7/4*x+1/3").get().coefficient(1), "-7/4");
+        aeq(read("x^2-7/4*x+1/3").get().coefficient(2), 1);
+        aeq(read("x^2-7/4*x+1/3").get().coefficient(3), 0);
+        aeq(read("x^3-1").get().coefficient(0), -1);
+        aeq(read("x^3-1").get().coefficient(1), 0);
+        aeq(read("x^3-1").get().coefficient(2), 0);
+        aeq(read("x^3-1").get().coefficient(3), 1);
+        aeq(read("x^3-1").get().coefficient(4), 0);
+        try {
+            read("x^3-1").get().coefficient(-1);
+            fail();
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
     }
 
     private static void aeq(Object a, Object b) {
