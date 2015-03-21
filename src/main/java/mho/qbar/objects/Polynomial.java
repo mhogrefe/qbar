f2aa5496c954c7f658ed35b1e239e02baa90a0e3
mhogrefe <mikhailhogrefe@gmail.com>
2015-03-20 22:03:18 -0400

    added polynomial constants

--- src/main/java/mho/qbar/objects/Polynomial.java
+++ src/main/java/mho/qbar/objects/Polynomial.java
@@ -3,9 +3,26 @@ package mho.qbar.objects;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Polynomial {
+    /**
+     * 0
+     */
+    public static final @NotNull Polynomial ZERO = new Polynomial(new ArrayList<>());
+
+    /**
+     * 1
+     */
+    public static final @NotNull Polynomial ONE = new Polynomial(Arrays.asList(BigInteger.ONE));
+
+    /**
+     * x
+     */
+    public static final @NotNull Polynomial X = new Polynomial(Arrays.asList(BigInteger.ZERO, BigInteger.ONE));
+
     private final @NotNull List<BigInteger> coefficients;
 
     private Polynomial(@NotNull List<BigInteger> coefficients) {
--- src/main/java/mho/qbar/objects/RationalPolynomial.java
+++ src/main/java/mho/qbar/objects/RationalPolynomial.java
@@ -2,9 +2,28 @@ package mho.qbar.objects;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RationalPolynomial {
+    /**
+     * 0
+     */
+    public static final @NotNull RationalPolynomial ZERO = new RationalPolynomial(new ArrayList<>());
+
+    /**
+     * 1
+     */
+    public static final @NotNull RationalPolynomial ONE = new RationalPolynomial(Arrays.asList(Rational.ONE));
+
+    /**
+     * x
+     */
+    public static final @NotNull RationalPolynomial X = new RationalPolynomial(
+            Arrays.asList(Rational.ZERO, Rational.ONE)
+    );
+
     private final @NotNull List<Rational> coefficients;
 
     private RationalPolynomial(@NotNull List<Rational> coefficients) {
