e031669f7180808e83e6502cfc5015b01baca6ea
mhogrefe <mikhailhogrefe@gmail.com>
2015-03-21 02:25:55 -0400

    added iterator tests

--- src/test/java/mho/qbar/objects/PolynomialTest.java
+++ src/test/java/mho/qbar/objects/PolynomialTest.java
@@ -3,6 +3,7 @@ package mho.qbar.objects;
 import org.junit.Test;
 
 import static mho.qbar.objects.Polynomial.*;
+import static mho.wheels.iterables.IterableUtils.*;
 import static org.junit.Assert.assertEquals;
 
 public class PolynomialTest {
@@ -11,6 +12,17 @@ public class PolynomialTest {
         aeq(ZERO, "0");
         aeq(ONE, "1");
         aeq(X, "x");
+    }
+
+    @Test
+    public void testIterator() {
+        aeq(toList(ZERO), "[]");
+        aeq(toList(ONE), "[1]");
+        aeq(toList(X), "[0, 1]");
+        aeq(toList(read("-17").get()), "[-17]");
+        aeq(toList(read("x^2-4*x+7").get()), "[7, -4, 1]");
+        aeq(toList(read("x^3-1").get()), "[-1, 0, 0, 1]");
+        aeq(toList(read("3*x^10").get()), "[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3]");
     }
 
     private static void aeq(Object a, Object b) {
--- src/test/java/mho/qbar/objects/RationalPolynomialTest.java
+++ src/test/java/mho/qbar/objects/RationalPolynomialTest.java
@@ -3,6 +3,7 @@ package mho.qbar.objects;
 import org.junit.Test;
 
 import static mho.qbar.objects.RationalPolynomial.*;
+import static mho.wheels.iterables.IterableUtils.*;
 import static org.junit.Assert.assertEquals;
 
 public class RationalPolynomialTest {
@@ -11,6 +12,17 @@ public class RationalPolynomialTest {
         aeq(ZERO, "0");
         aeq(ONE, "1");
         aeq(X, "x");
+    }
+
+    @Test
+    public void testIterator() {
+        aeq(toList(ZERO), "[]");
+        aeq(toList(ONE), "[1]");
+        aeq(toList(X), "[0, 1]");
+        aeq(toList(read("-4/3").get()), "[-4/3]");
+        aeq(toList(read("x^2-7/4*x+1/3").get()), "[1/3, -7/4, 1]");
+        aeq(toList(read("x^3-1").get()), "[-1, 0, 0, 1]");
+        aeq(toList(read("1/2*x^10").get()), "[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1/2]");
     }
 
     private static void aeq(Object a, Object b) {
