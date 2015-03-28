3fd689d350fd47bfbe0b69f3b4c827989f712898
mhogrefe <mikhailhogrefe@gmail.com>
2015-03-28 18:42:52 -0400

    started RationalMatrix

---
+++ src/test/java/mho/qbar/objects/RationalMatrix.java
@@ -0,0 +1,11 @@
+package mho.qbar.objects;
+
+/**
+ * <p>A matrix with {@link Rational} elements. The number of rows is the height, and the number of rows is the width.
+ * Either the height or the width, or both, may be 0; 0-width matrices are written {@code "[]#w"}, where {@code w} is
+ * the width. When referring to elements, the (i, j)th element is the element in row i and column j.
+ *
+ * <p>This class is immutable.
+ */
+public class RationalMatrix {
+}
