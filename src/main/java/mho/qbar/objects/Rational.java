7fbbb6e712a883f3515ec96025634045114003fd
mhogrefe <mikhailhogrefe@gmail.com>
2015-05-31 21:36:55 -0400

    clarify docs

--- src/main/java/mho/qbar/objects/Rational.java
+++ src/main/java/mho/qbar/objects/Rational.java
@@ -610,7 +610,7 @@ public final class Rational implements Comparable<Rational> {
      *  terminating; that is, its denominator must only have 2 or 5 as prime factors.</li>
      *  <li>If {@code roundingMode} is {@code RoundingMode.UNNECESSARY}, then {@code precision} must be at least as
      *  large as the number of digits in {@code this}'s decimal expansion.</li>
-     *  <li>The result is a non-null.</li>
+     *  <li>The result is non-null.</li>
      * </ul>
      *
      * @param precision the precision with which to round {@code this}. 0 indicates full precision.
@@ -635,8 +635,8 @@ public final class Rational implements Comparable<Rational> {
      *  <li>{@code precision} cannot be negative.</li>
      *  <li>If {@code precision} is 0, then {@code this} must be a {@code Rational} whose decimal expansion is
      *  terminating; that is, its denominator must only have 2 or 5 as prime factors.</li>
-     *  <li>The result is a {@code BigDecimal} x such that x's scale is greater than or equal to zero and less than or
-     *  equal to n, where n is the smallest non-negative integer such that x×10<sup>n</sup> is an integer.</li>
+     *  <li>The result is a {@code BigDecimal} with minimal scale. That is, the scale is the smallest non-negative n
+     *  such that {@code this}×10<sup>n</sup> is an integer.</li>
      * </ul>
      *
      * @param precision the precision with which to round {@code this}. 0 indicates full precision.
