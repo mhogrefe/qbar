510a9a5a22d2b31237c69ee550689c5cdf5a6c13
mhogrefe <mikhailhogrefe@gmail.com>
2015-11-04 00:26:14 -0500

    fixed subset type signature

--- src/main/java/mho/qbar/iterableProviders/QBarIterableProvider.java
+++ src/main/java/mho/qbar/iterableProviders/QBarIterableProvider.java
@@ -2388,7 +2388,7 @@ public strictfp abstract class QBarIterableProvider {
      * @param xs an {@code Iterable} of elements
      * @param <T> the type of values in the {@code List}s
      */
-    public @NotNull <T> Iterable<List<T>> subsets(int size, @NotNull Iterable<T> xs) {
+    public @NotNull <T extends Comparable<T>> Iterable<List<T>> subsets(int size, @NotNull Iterable<T> xs) {
         return wheelsProvider.subsets(size, xs);
     }
 
