8026dbbac95658ecfb7d0c7d370347336079174e
mhogrefe <mikhailhogrefe@gmail.com>
2015-11-25 00:02:32 -0500

    finished Rational range demos

--- src/test/java/mho/qbar/iterableProviders/QBarRandomProviderDemos.java
+++ src/test/java/mho/qbar/iterableProviders/QBarRandomProviderDemos.java
@@ -1,10 +1,11 @@
 package mho.qbar.iterableProviders;
 
 import mho.qbar.objects.Rational;
-import mho.wheels.iterables.RandomProvider;
 import mho.wheels.structures.Pair;
+import mho.wheels.structures.Triple;
 
 import static mho.wheels.iterables.IterableUtils.*;
+import static mho.wheels.ordering.Ordering.*;
 import static mho.wheels.testing.Testing.*;
 
 @SuppressWarnings("UnusedDeclaration")
@@ -23,6 +24,10 @@ public class QBarRandomProviderDemos {
             P = QBarExhaustiveProvider.INSTANCE;
             LIMIT = 10000;
         }
+    }
+
+    public static void main(String[] args) {
+        demoRange_Rational_Rational();
     }
 
     private static void demoPositiveRationals() {
@@ -100,6 +105,21 @@ public class QBarRandomProviderDemos {
         );
         for (Pair<QBarRandomProvider, Rational> p : take(SMALL_LIMIT, ps)) {
             System.out.println("rangeDown(" + p.a + ", " + p.b + ") = " + its(p.a.rangeDown(p.b)));
+        }
+    }
+
+    private static void demoRange_Rational_Rational() {
+        initialize();
+        Iterable<Triple<QBarRandomProvider, Rational, Rational>> ts = filterInfinite(
+                t -> le(t.b, t.c),
+                P.triples(
+                        filterInfinite(rp -> rp.getScale() >= 4, P.qbarRandomProvidersDefaultSecondaryScale()),
+                        P.rationals(),
+                        P.rationals()
+                )
+        );
+        for (Triple<QBarRandomProvider, Rational, Rational> t : take(SMALL_LIMIT, ts)) {
+            System.out.println("range(" + t.a + ", " + t.b + ", " + t.c + ") = " + its(t.a.range(t.b, t.c)));
         }
     }
 }
