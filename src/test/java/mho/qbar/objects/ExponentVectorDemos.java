cb1f6ba1b57529034583c409594af404bc415984
mhogrefe <mikhailhogrefe@gmail.com>
2016-02-20 13:42:25 -0500

    finished demos for existing ExponentVector methods

--- src/test/java/mho/qbar/objects/ExponentVectorDemos.java
+++ src/test/java/mho/qbar/objects/ExponentVectorDemos.java
@@ -1,6 +1,7 @@
 package mho.qbar.objects;
 
 import mho.qbar.testing.QBarDemos;
+import mho.wheels.ordering.Ordering;
 import mho.wheels.structures.Pair;
 import org.jetbrains.annotations.NotNull;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 import static mho.qbar.objects.ExponentVector.*;
 import static mho.wheels.iterables.IterableUtils.*;
+import static mho.wheels.testing.Testing.nicePrint;
 
 @SuppressWarnings("UnusedDeclaration")
 public class ExponentVectorDemos extends QBarDemos {
@@ -25,10 +27,7 @@ public class ExponentVectorDemos extends QBarDemos {
     }
 
     private void demoExponent() {
-        Iterable<Pair<ExponentVector, Variable>> ps = P.pairsLogarithmicOrder(
-                P.withScale(4).exponentVectors(),
-                P.withScale(4).variables()
-        );
+        Iterable<Pair<ExponentVector, Variable>> ps = P.pairsLogarithmicOrder(P.exponentVectors(), P.variables());
         for (Pair<ExponentVector, Variable> p : take(LIMIT, ps)) {
             System.out.println("exponent(" + p.a + ", " + p.b + ") = " + p.a.exponent(p.b));
         }
@@ -47,7 +46,7 @@ public class ExponentVectorDemos extends QBarDemos {
     }
 
     private void demoOf() {
-        for (List<Integer> is : take(LIMIT, P.withScale(4).lists(P.naturalIntegersGeometric()))) {
+        for (List<Integer> is : take(LIMIT, P.lists(P.naturalIntegersGeometric()))) {
             String listString = tail(init(is.toString()));
             System.out.println("of(" + listString + ") = " + of(is));
         }
@@ -59,10 +58,10 @@ public class ExponentVectorDemos extends QBarDemos {
                 map(
                         p -> toList(zip(p.a, p.b)),
                         P.dependentPairsInfinite(
-                                P.withScale(4).subsetsAtLeast(1, P.withScale(4).variables()),
+                                P.subsetsAtLeast(1, P.variables()),
                                 vs -> filterInfinite(
                                         is -> last(is) != 0,
-                                        P.lists(vs.size(), P.withScale(4).positiveIntegersGeometric())
+                                        P.lists(vs.size(), P.positiveIntegersGeometric())
                                 )
                         )
                 )
@@ -73,8 +72,63 @@ public class ExponentVectorDemos extends QBarDemos {
         }
     }
 
+    private void demoDegree() {
+        for (ExponentVector ev : take(LIMIT, P.exponentVectors())) {
+            System.out.println("degree(" + ev + ") = " + ev.degree());
+        }
+    }
+
+    private void demoEquals_ExponentVector() {
+        for (Pair<ExponentVector, ExponentVector> p : take(LIMIT, P.pairs(P.exponentVectors()))) {
+            System.out.println(p.a + (p.a.equals(p.b) ? " = " : " ≠ ") + p.b);
+        }
+    }
+
+    private void demoEquals_null() {
+        for (ExponentVector ev : take(LIMIT, P.exponentVectors())) {
+            //noinspection ObjectEqualsNull
+            System.out.println(ev + (ev.equals(null) ? " = " : " ≠ ") + null);
+        }
+    }
+
+    private void demoHashCode() {
+        for (ExponentVector ev : take(LIMIT, P.exponentVectors())) {
+            System.out.println("hashCode(" + ev + ") = " + ev.hashCode());
+        }
+    }
+
+    private void demoCompareTo() {
+        for (Pair<ExponentVector, ExponentVector> p : take(LIMIT, P.pairs(P.exponentVectors()))) {
+            System.out.println(p.a + " " + Ordering.compare(p.a, p.b).toChar() + " " + p.b);
+        }
+    }
+
+    private void demoRead() {
+        for (String s : take(LIMIT, P.strings())) {
+            System.out.println("read(" + nicePrint(s) + ") = " + read(s));
+        }
+    }
+
+    private void demoRead_targeted() {
+        for (String s : take(LIMIT, P.strings(EXPONENT_VECTOR_CHARS))) {
+            System.out.println("read(" + s + ") = " + read(s));
+        }
+    }
+
+    private void demoFindIn() {
+        for (String s : take(LIMIT, P.strings())) {
+            System.out.println("findIn(" + nicePrint(s) + ") = " + findIn(s));
+        }
+    }
+
+    private void demoFindIn_targeted() {
+        for (String s : take(LIMIT, P.strings(EXPONENT_VECTOR_CHARS))) {
+            System.out.println("findIn(" + s + ") = " + findIn(s));
+        }
+    }
+
     private void demoToString() {
-        for (ExponentVector ev : take(LIMIT, P.withScale(4).exponentVectors())) {
+        for (ExponentVector ev : take(LIMIT, P.exponentVectors())) {
             System.out.println(ev);
         }
     }
