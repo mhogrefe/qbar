c2a985615e88e1275c30c07266690cc2bb0db207
mhogrefe <mikhailhogrefe@gmail.com>
2016-02-28 17:53:05 -0500

    added more MultivariatePolynomial methods

--- src/main/java/mho/qbar/objects/MultivariatePolynomial.java
+++ src/main/java/mho/qbar/objects/MultivariatePolynomial.java
@@ -1,15 +1,13 @@
 package mho.qbar.objects;
 
 import mho.wheels.io.Readers;
+import mho.wheels.iterables.NoRemoveIterable;
 import mho.wheels.numberUtils.IntegerUtils;
 import mho.wheels.structures.Pair;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static mho.wheels.iterables.IterableUtils.*;
 import static mho.wheels.testing.Testing.assertTrue;
@@ -20,11 +18,11 @@ import static mho.wheels.testing.Testing.assertTrue;
  * <p>There is only one instance of {@code ZERO}, and one instance of {@code ONE}, so these may be compared with other
  * {@code MultivariatePolynomial}s using {@code ==}.</p>
  *
- * <p></p>
- *
  * <p>This class is immutable.</p>
  */
-public class MultivariatePolynomial {
+public class MultivariatePolynomial implements
+        Comparable<MultivariatePolynomial>,
+        Iterable<Pair<BigInteger, ExponentVector>> {
     /**
      * 0
      */
@@ -36,10 +34,42 @@ public class MultivariatePolynomial {
     public static final @NotNull MultivariatePolynomial ONE =
             new MultivariatePolynomial(Collections.singletonList(new Pair<>(BigInteger.ONE, ExponentVector.ONE)));
 
+    /**
+     * This {@code MultivariatePolynomial}'s terms. The first element of each pair is the coefficient of the
+     * {@code ExponentVector} in the second slot. The terms are in grevlex order.
+     */
     private final @NotNull List<Pair<BigInteger, ExponentVector>> terms;
 
+    /**
+     * Private constructor for {@code MultivariatePolynomial}; assumes argument is valid
+     *
+     * <ul>
+     *  <li>{@code terms} cannot have any null elements. The {@code ExponentVector}s must be unique and in grevlex
+     *  order.</li>
+     *  <li>Any {@code MultivariatePolynomial} may be constructed with this constructor.</li>
+     * </ul>
+     *
+     * @param terms the polynomial's terms
+     */
     private MultivariatePolynomial(@NotNull List<Pair<BigInteger, ExponentVector>> terms) {
         this.terms = terms;
+    }
+
+    /**
+     * Returns an {@code Iterator} over this {@code MultivariatePolynomial}'s terms, from lowest to highest in grevlex
+     * ordering. Does not support removal.
+     *
+     * <ul>
+     *  <li>{@code this} may be any {@code MultivariatePolynomial}.</li>
+     *  <li>The result is finite, and contains no nulls. The {@code ExponentVector}s are in increasing grevlex
+     *  order.</li>
+     * </ul>
+     *
+     * @return an {@code Iterator} over this {@code MultivariatePolynomial}'s terms
+     */
+    @Override
+    public @NotNull Iterator<Pair<BigInteger, ExponentVector>> iterator() {
+        return new NoRemoveIterable<>(terms).iterator();
     }
 
     /**
@@ -75,6 +105,55 @@ public class MultivariatePolynomial {
         return terms.hashCode();
     }
 
+    /**
+     * Compares {@code this} to {@code that}, returning 1, –1, or 0 if the answer is "greater than", "less than", or
+     * "equal to", respectively. The terms are compared lexicographically, starting from the end of the {@code term}s
+     * lists.
+     *
+     * <ul>
+     *  <li>{@code this} may be any {@code MultivariatePolynomial}.</li>
+     *  <li>{@code that} cannot be null.</li>
+     *  <li>The result may be –1, 0, or 1.</li>
+     * </ul>
+     *
+     * @param that the {@code Polynomial} to be compared with {@code this}
+     * @return {@code this} compared to {@code that}
+     */
+    @Override
+    public int compareTo(@NotNull MultivariatePolynomial that) {
+        if (this == that) return 0;
+        if (this == ZERO) return -1;
+        if (that == ZERO) return 1;
+        int i = terms.size() - 1;
+        int j = that.terms.size() - 1;
+        while (i >= 0 && j >= 0) {
+            Pair<BigInteger, ExponentVector> thisTerm = terms.get(i);
+            Pair<BigInteger, ExponentVector> thatTerm = that.terms.get(i);
+            int evCompare = thisTerm.b.compareTo(thatTerm.b);
+            if (evCompare != 0) return evCompare;
+            int iCompare = thisTerm.a.compareTo(thisTerm.a);
+            if (iCompare != 0) return iCompare;
+            i--;
+            j--;
+        }
+        if (i < 0) return -1;
+        if (j < 0) return 1;
+        return 0;
+    }
+
+    /**
+     * Creates a {@code MultivariatePolynomial} from a {@code String}. Valid input takes the form of a {@code String}
+     * that could have been returned by {@link MultivariatePolynomial#toString()}.
+     *
+     * <ul>
+     *  <li>{@code s} must be non-null.</li>
+     *  <li>The result may contain any {@code MultivariatePolynomial}, or be empty.</li>
+     * </ul>
+     *
+     * @param s a string representation of a {@code MultivariatePolynomial}
+     * @return the {@code MultivariatePolynomial} represented by {@code s}, or an empty {@code Optional} if {@code s}
+     * is invalid
+     */
     private static @NotNull Optional<MultivariatePolynomial> read(@NotNull String s) {
         if (s.equals("0")) return Optional.of(ZERO);
         if (s.equals("1")) return Optional.of(ONE);
@@ -149,6 +228,27 @@ public class MultivariatePolynomial {
         //noinspection RedundantCast
         if (!increasing((Iterable<ExponentVector>) map(t -> t.b, terms))) return Optional.empty();
         return Optional.of(new MultivariatePolynomial(terms));
+    }
+
+    /**
+     * Finds the first occurrence of a {@code MultivariatePolynomial} in a {@code String}. Returns the
+     * {@code MultivariatePolynomial} and the index at which it was found. Returns an empty {@code Optional} if no
+     * {@code MultivariatePolynomial} is found. Only {@code String}s which could have been emitted by
+     * {@link MultivariatePolynomial#toString} are recognized. The longest possible {@code MultivariatePolynomial} is
+     * parsed.
+     *
+     * <ul>
+     *  <li>{@code s} must be non-null.</li>
+     *  <li>The result is non-null. If it is non-empty, then neither of the {@code Pair}'s components is null, and the
+     *  second component is non-negative.</li>
+     * </ul>
+     *
+     * @param s the input {@code String}
+     * @return the first {@code MultivariatePolynomial} found in {@code s}, and the index at which it was found
+     */
+    public static @NotNull Optional<Pair<MultivariatePolynomial, Integer>> findIn(@NotNull String s) {
+        return Readers.genericFindIn(MultivariatePolynomial::read, "*+-0123456789^abcdefghijklmnopqrstuvwxyz")
+                .apply(s);
     }
 
     /**
